73833472c6c953ab57b6c513028db833579efa0b
Quentin SENTE <quentin.sente@condorcet.be>
2020-12-31 00:24:48 +0100

    hdshdskd

---
+++ src/be/sentedagnely/API/ConnectionDB.java
@@ -0,0 +1,10 @@
+package be.sentedagnely.API;
+
+import java.sql.Connection;
+
+public class ConnectionDB {
+	private static Connection connect = null;
+	
+	private ConnectionDB() {}
+	// a finir
+}
